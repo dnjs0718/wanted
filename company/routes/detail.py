12bf120efcc1a98f8ee2e9c9c819b87f265d02b0
dnjs0718 <mystund@naver.com>
2024-08-12 16:19:41 +0900

    [FEATURE] 회사 이름으로 회사 검색

--- company/routes/detail.py
+++ company/routes/detail.py
@@ -15,6 +15,32 @@ class CompanyDetailResponse(BaseModel):
 
 @router.get("", response_model=CompanyDetailResponse, summary="회사 이름으로 회사 검색")
 async def company_detail(request: Request, company_name: str):
+    """
+    # Author
+    - 원재연
+    
+    # Description
+    - 회사 이름으로 회사 검색
+    
+    # Request Header
+    - x-wanted-language : str = 원하는 언어
+    
+    # Request Query Parameter
+    - company_name : str = 회사명
+    
+    # Response
+    ```
+    {
+        "company_name": str = 회사명,
+        "tags": list(str) = 태그 리스트
+    }
+    ```
+    
+    # Error
+    - 401 : 사용 언어를 헤더에 담지 않았을 때
+    - 404 : 사용 언어를 지원하지 않을 때 (language not found)
+    - 404 : 회사를 찾을 수 없을 때 (company not found)
+    """
     language = request.state.language
     if not (
         company_language := await CompanyLanguage.get_or_none(
